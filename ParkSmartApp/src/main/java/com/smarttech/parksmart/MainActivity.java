04abf8e7bd77347520696e7c1e21c9a0ec2790ef
VijayMatadeen <71228563+VijayMatadeen@users.noreply.github.com>
2020-11-17 01:37:32 -0500

    Connenect to FireBase and Finished GPS

--- ParkSmartApp/src/main/java/com/smarttech/parksmart/MainActivity.java
+++ ParkSmartApp/src/main/java/com/smarttech/parksmart/MainActivity.java
@@ -1,18 +1,84 @@
 package com.smarttech.parksmart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton launch_Button;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Sets up the splash screen
         setTheme(R.style.AppTheme);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //Getting Reference to Root Node
+        DatabaseReference myRef = database.getReference();
+        //Getting reference to "child 1" node
+        myRef = myRef.child("Coordinates");
+        //myRef.setValue("Vijay Matadeen");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String x = dataSnapshot.child("x").getValue().toString();
+                String y = dataSnapshot.child("y").getValue().toString();
+
+                Toast.makeText(MainActivity.this, "Data Received: " + y, Toast.LENGTH_SHORT).show();
+
+                String beginning = "google.navigation:q=";
+                String comma = ",";
+                String end = "&mode=d";
+
+                final String URI_GPS = beginning + x + comma + y + end;
+
+
+                launch_Button = findViewById(R.id.direction);
+
+                launch_Button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Intent intent = new Intent(Intent.ACTION_VIEW,
+                                Uri.parse(URI_GPS));
+
+                        intent.setPackage("com.google.android.apps.maps");
+                        //Intent chooser = Intent.createChooser(intent,"Launch Maps");
+
+                        if(intent.resolveActivity(getPackageManager()) != null) {
+                            startActivity(intent);
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled: Something went wrong! Error:" + databaseError.getMessage() );
+            }
+        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -26,5 +92,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
