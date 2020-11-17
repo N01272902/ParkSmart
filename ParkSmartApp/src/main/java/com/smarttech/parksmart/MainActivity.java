94cea0e1b83202917ae2a6314e58e4fa659fa9e1
N01272902 <n01272902@humbermail.ca>
2020-11-17 18:14:15 -0500

    OnBackPressed functionality setup

--- ParkSmartApp/src/main/java/com/smarttech/parksmart/MainActivity.java
+++ ParkSmartApp/src/main/java/com/smarttech/parksmart/MainActivity.java
@@ -1,9 +1,11 @@
 package com.smarttech.parksmart;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -55,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+//Database connection setup
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         //Getting Reference to Root Node
@@ -102,8 +104,29 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, "onCancelled: Something went wrong! Error:" + databaseError.getMessage() );
             }
         });
-
-
-
     }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Do you want to Exit?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user pressed "yes", then he is allowed to exit from application
+                finish();
+            }
+        });
+        builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user select "No", just cancel this dialog and continue with app
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert=builder.create();
+        alert.show();
+    }
+
 }
