30fadd951fdb6d6efa53ac9eccbb2c81766d30f1
Mandeep2310 <71230920+Mandeep2310@users.noreply.github.com>
2020-11-18 16:49:55 -0500

    added a google-sigin button to the login screen

--- ParkSmartApp/src/main/java/com/smarttech/parksmart/SignUpActivity.java
+++ ParkSmartApp/src/main/java/com/smarttech/parksmart/SignUpActivity.java
@@ -10,6 +10,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -103,5 +107,16 @@ public class SignUpActivity extends AppCompatActivity {
 
             }
         });
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        //Build a GoogleSignInClient with the options specified by gso
+        GoogleSignInClient mGoogleSignInClient= GoogleSignIn.getClient(this,gso);
+
+        //Check for existing Google Sign In account, if the user is already signed in
+        //the GoogleSigInAccount will be non-null
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
     }
 }
