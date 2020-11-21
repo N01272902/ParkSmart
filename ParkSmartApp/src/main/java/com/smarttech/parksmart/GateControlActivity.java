2c640e1d5e9242495f01124633905f60b49e7f8f
VijayMatadeen <71228563+VijayMatadeen@users.noreply.github.com>
2020-11-21 17:26:50 -0500

    Set TextView to allow user to see whehter the gate is open or close from the database

--- ParkSmartApp/src/main/java/com/smarttech/parksmart/GateControlActivity.java
+++ ParkSmartApp/src/main/java/com/smarttech/parksmart/GateControlActivity.java
@@ -6,21 +6,53 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class GateControlActivity extends AppCompatActivity {
+
+    TextView Gate1;
+    TextView Gate2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gate_control);
+
+        Gate1=(TextView)findViewById(R.id.StatusGate1);
+        Gate2=(TextView)findViewById(R.id.StatusGate2);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //Getting Reference to Root Node
+        DatabaseReference myRef = database.getReference();
+        myRef = myRef.child("Gate_Control");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String EntryGate = (String) dataSnapshot.child("Gate_1").getValue();
+                Gate1.setText(EntryGate);
+                String ExitGate = (String) dataSnapshot.child("Gate_2").getValue();
+                Gate2.setText(ExitGate);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         Button GateOpen1= findViewById(R.id.openGateButton1);
         GateOpen1.setOnClickListener(new View.OnClickListener() {
