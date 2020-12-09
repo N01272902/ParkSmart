ab1f77d6d0751f1836a8005c75df761684214640
N01272902 <n01272902@humbermail.ca>
2020-12-08 21:28:57 -0500

    Notifications added (Updates from firebase cloud messaging).

---
+++ ParkSmartApp/src/main/java/com/smarttech/parksmart/services/MyFirebaseInstanceService.java
@@ -0,0 +1,103 @@
+package com.smarttech.parksmart.services;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.smarttech.parksmart.R;
+
+import java.util.Map;
+import java.util.Random;
+
+//App Notifications Setup
+public class MyFirebaseInstanceService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        if (remoteMessage.getData().isEmpty()) {
+
+            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        } else {
+            showNotification(remoteMessage.getData());
+
+        }
+
+    }
+
+    private void showNotification(Map<String, String> data) {
+        String title = data.get("title").toString();
+        String body = data.get("body").toString();
+
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "smarttech.parksmart.services.test";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("Park Smart");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_park_notif)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info");
+
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
+
+    }
+
+    private void showNotification(String title, String body) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "smarttech.parksmart.services.test";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("Park Smart");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_park_notif)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info");
+
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+
+        Log.d("TOKENFIREBASE", s);
+    }
+}
